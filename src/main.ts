1d705ba32b3c4b29886150cce490ea8932406aa0
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-23 16:36:52 +0200

    Use morgan logging middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import * as express from 'express';
+import * as morgan from 'morgan';
 
 import {WelcomeController} from './routes/welcome';
 
 const app: express.Application = express();
 const port: any = process.env.PORT || 3000;
+
+const morgan: morgan.Morgan = morgan('combined')
+
+app.use(morgan);
 
 app.use('/welcome', WelcomeController);
 
