4ae50af251344648c0b056ac2906a00b2e473959
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-23 14:06:12 +0200

    Added first versions

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-const express: any = require('express')
+import * as express from 'express';
+
+import {WelcomeController} from './routes/welcome';
+
+const app: express.Application = express();
+const port: any = process.env.PORT || 3000;
+
+app.use('/welcome', WelcomeController);
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/`);
+    console.log(app._router.stack)
+});
