debfa1c107d9cc2c2c373257097a31d251efb886
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-23 17:04:58 +0200

    Add math lib + tests

---
+++ test/nodebb-math.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var math   = require('./../dist/lib/math.js')
+describe('Math', function() {
+  describe('#add(a, b)', function() {
+    it('should return 0 when giving 0 and 0 as arguments', function() {
+      assert.equal(math.add(0, 0), 0);
+    });
+    it('should return 2 when giving 1 and 1 as arguments', function() {
+      assert.equal(math.add(1, 1), 2);
+    });
+    it('should return 3 when giving 1 and 2 as arguments', function() {
+      assert.equal(math.add(1, 2), 3);
+    });
+    it('should return 4 when giving 2 and 2 as arguments', function() {
+      assert.equal(math.add(2, 2), 4);
+    });
+  });
+});
